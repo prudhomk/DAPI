e62ea8b8210b758bede9f47b3e8d5a7da5288f7b
Kalan Prudhomme <cleaver9@gmail.com>
2021-12-23 10:38:43 -0800

    adding seed data

--- data/digimonData.js
+++ data/digimonData.js
@@ -7,7 +7,305 @@ const digimons = [
     type: 'Lizard', 
     canDigivolve: true,
     tamer: 'Taichi "Tai" Kamiya'
-  }
+  },
+  {
+    name: 'Gabumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Biyomon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Tentomon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Palmon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Gomamon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Patamon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Veemon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Wormmon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Hawkmon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Armadillomon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Monodramon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Meicoomon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Meramon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Andromon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Monzaemon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Leomon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Frigimon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Mojyamon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Centarumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Elecmon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Whamon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Piximon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Wizardmon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+  {
+    name: 'Agumon', 
+    series: 'Adventure 01',
+    level: 'Rookie', 
+    gender: 'Male', 
+    type: 'Lizard', 
+    canDigivolve: true,
+    tamer: 'Taichi "Tai" Kamiya'
+  },
+
 ];
 
 export default digimons;
